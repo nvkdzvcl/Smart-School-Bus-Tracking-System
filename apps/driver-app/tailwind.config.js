8cdad47edb7cea12952d99be8be63d65b3bc7002
Nguyễn Văn Khanh <3123560042@sv.sgu.edu.vn>
2025-10-27 21:48:08 +0700

    ->react 18

--- apps/driver-app/tailwind.config.js
+++ apps/driver-app/tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate"
 
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   darkMode: "class",
   content: [
     "./index.html",
@@ -57,5 +58,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-};
+  plugins: [tailwindcssAnimate],
+}
